392627a51723b7db94e0f2019f8a8e83e3d23e6f
silviakago <deejaykayslyk@gmail.com>
2020-08-27 19:54:45 +0300

    add modified stracture

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
-function getAkanName(){
-    var days = ["sunday","Monday","Tuseday","Wednesday","Thursday","Friday","Saturday"];
-    var maleAkanNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
-    var femaleAkanNames = ["Akosua","Adowoa"."Abenaa","Akua","Yaa"."Afua","Ama"]
-    var myBirthday = document.getElementById("myBirthDate").Value;
-    var myGender = document.getElementsByName("gender");
-    var dateOfBirth = new Date(myBirthday);
-    var dayOfTheweek = dateOfBirth.getDay();
-     if(myBirthday === ""){
-         document.getElementById('message').innerHTML = "<div class=\"alert alert-danger\"><button type=\"button\" class=\"class
-         $('#message').addClass("animated shake");
+var akanMale = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
+var akanFemale = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
+var dayOfTheWeek =["Sunday","Monday","Tuesday","Wednsday","Thursday","Friday","Saturday"];
+function checkgender() {
+    var month =parseInt(document.getElementById("month").value);
+    var year =parseInt(document.getElementById("year").value);
+    var day=parseInt(document.getElementById("day").value);
+    var date=new Date(year + "/" + month + "/" + day);
+    var gender=checkout();
+    var day2=date.getDay();
+    var akan;
+    if(day <1 || day >31){
+        alert("invalid date");
+            return false;
+    }
+    if (month <1 || month>12){
+        alert("Invalid month");
+        return false;
+    }
+    if(year <1900 || year >2019){
+            alert("invalid year");
+            return false;
+    }
+    if(gender === "male") {
+        akan =akanMale[day2];
+    }else{
+            akan =akanFemale[day2];
+    }
+            alert("You were born on " + dayOfTheWeek [day2] + "and your akan name is " + akan);
+}
+function checkout(){
+    var gender=document.getElementsByName("gender");
+    for(i=0; i<gender.length; i++){
+        if(gender[i].checked){
+            return(gender[i].value)
+        }
+    }
 }
